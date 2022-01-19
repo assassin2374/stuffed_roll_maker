8de9ac9a086c59a26651d05f38a0d070704e7e11
assassin_box <assassin543@outlook.jp>
2022-01-20 01:31:10 +0900

    スクロールできるように修正

--- src/view/Preview.tsx
+++ src/view/Preview.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Stuff } from "../model/stuff";
+import { useEffect, useState } from "react";
 
 const sampleStuffRoll: Stuff[] = [
   {
@@ -21,22 +22,55 @@ const sampleStuffRoll: Stuff[] = [
 ];
 
 const Preview: React.FC = () => {
+  const [roll, setRoll] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRoll((roll) => {
+        return roll + 1;
+      });
+    }, 100);
+    return () => {
+      return clearInterval(interval);
+    };
+  }, []);
+
+  const countUp = () => {
+    setCount(count + 1);
+    return count;
+  };
+
   return (
     <div
       style={{
         width: "100%",
-        height: "100%",
+        height: "100vh",
         backgroundColor: "black",
         margin: 0,
       }}
     >
-      {sampleStuffRoll.map((stuff) => {
-        return (
-          <div key={stuff.name} style={{ fontSize: 100, color: "white" }}>
-            {stuff.role}:{stuff.name}
-          </div>
-        );
-      })}
+      <div style={{ position: "absolute", top: roll }}>
+        {sampleStuffRoll.map((stuff) => {
+          return (
+            <div key={countUp()}>
+              <div style={{ fontSize: 50, color: "red" }}>
+                {stuff.role} {stuff.name}
+              </div>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
